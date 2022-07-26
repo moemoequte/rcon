b8efaa87c4d7087d2ff997370786ef20902f78dd
luckyweNda <aluckywenda@gmail.com>
2022-07-26 08:27:53 +0800

    fix double free

--- src/rcon_connection.cpp
+++ src/rcon_connection.cpp
@@ -24,14 +24,19 @@ void connection::authenticate(std::string password)
 std::string connection::send_packet(std::string cmd, int32_t type)
 {
     int32_t packet_size = 4 + 4 + 4 + cmd.length() + 1 + 1;
-    char packet[packet_size];
-    packet[0] = packet_size - 4;
-    packet[4] = ++connection::packet_id_;
-    packet[8] = type;
+    unsigned char packet[packet_size];
+    for(size_t i = 0; i < packet_size; i++)
+        packet[i] = '\0';
+    for(uint16_t i = 0; i < 4; i++)
+        packet[i] = static_cast<unsigned char>((packet_size - 4) >> (i * 8));
+    for(uint16_t i = 0; i < 4; i++)
+        packet[4 + i] = static_cast<unsigned char>(connection::packet_id_ >> (i * 8));
+    for(uint16_t i = 0; i < 4; i++)
+        packet[8 + i] = static_cast<unsigned char>(type >> (i * 8));
     for(size_t i = 0; i < cmd.length(); i++)
-        packet[12 + i] = cmd[i];
-    packet[packet_size - 2] = packet[packet_size - 1] = '\0';
+        packet[12 + i] = static_cast<unsigned char>(cmd[i]);
     socket_.send(net::buffer(packet, packet_size));
+    return "";
 }
 
 } // namespace rcon
